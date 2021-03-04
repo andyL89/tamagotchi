2c39f2198df6c48ef51eaa2cfceed3ab48fe04d2
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-04 13:23:23 -0800

    Test elasped time method. Pass.
    Co-authored-by: Jo Miller <joannadawnmiller@gmail.com>
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    Co-authored-by: Andy Lawson <alawson89@gmail.com>

--- lib/tama.rb
+++ lib/tama.rb
@@ -1,11 +1,14 @@
+require 'pry'
+
 class Tamagotchi
-  attr_reader :name, :food_level, :sleep_level, :activity_level
+  attr_reader :name, :food_level, :sleep_level, :activity_level, :birth
 
   def initialize(name)
     @name = name
     @food_level = 10
     @sleep_level = 10
     @activity_level = 10
+    @birth = Time.now
   end
 
   def is_alive
@@ -13,8 +16,22 @@ class Tamagotchi
   end
 
   def set_food_level(number)
-    @food_level = number
+      @food_level += number
+      food_time = Time.new
   end
+
+  def time_passes(starting)
+    ending = Time.now
+    @elapsed = ((ending - starting) / 3600).floor
+    @elapsed
+  end
+
+  def check_food_level
+    if (time_passes(food_time)) > 1
+    set_food_level(-(@elapsed))
+    end
+  end
+
 end
 
 
--- spec/tama_spec.rb
+++ spec/tama_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require 'tama'
+require 'pry'
 
 describe(Tamagotchi) do
   # before(:each) do
@@ -20,10 +21,29 @@ describe(Tamagotchi) do
       my_pet = Tamagotchi.new("lil dragon")
       expect(my_pet.is_alive()).to(eq(true))
     end
+
     it("is dead if the food level is 0") do
       my_pet = Tamagotchi.new("lil dragon")
-      my_pet.set_food_level(0) 
+      my_pet.set_food_level(-10)
       expect(my_pet.is_alive()).to(eq(false))
     end
   end
+
+
+  describe("#time_passes") do
+  it("returns elapsed time since starting-time when called") do
+    my_pet = Tamagotchi.new("lil dragon")
+    expect(my_pet.time_passes(my_pet.birth)).to(eq(0))
+    end
+  end
+
+  # describe("#time_passes") do
+  # it("decreases the amount of food the Tamagotchi has left by 1") do
+  #   my_pet = Tamagotchi.new("lil dragon")
+  #   my_pet.time_passes(Time.now)  #decide what trigger you will use to make time pass
+  #   @elaped = 4
+  #   expect(my_pet.food_level()).to(eq(9))
+  #   end
+  # end
+
 end
